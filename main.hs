0686d8c6d591451709407214181d47b05a630fa6
Ramos <1joaoramos1@gmail.com>
2023-12-28 17:28:19 +0000

    Building Bool Expression

--- main.hs
+++ main.hs
@@ -150,7 +150,12 @@ testAssembler code = (stack2Str stack, state2Str state)
 -- testAssembler [Push (-20),Push (-21), Le] == ("True","")
 -- testAssembler [Push 5,Store "x",Push 1,Fetch "x",Sub,Store "x"] == ("","x=4")
 -- testAssembler [Push 10,Store "i",Push 1,Store "fact",Loop [Push 1,Fetch "i",Equ,Neg] [Fetch "i",Fetch "fact",Mult,Store "fact",Push 1,Fetch "i",Sub,Store "i"]] == ("","fact=3628800,i=1")
--- testAssembler [Push 1,Push 2,And] == "Run-time error"
+-- If you test:
+-- testAssembler [Push 1,Push 2,And]
+-- You should get an exception with the string: "Run-time error"
+-- If you test:
+-- testAssembler [Tru,Tru,Store "y", Fetch "x",Tru]
+-- You should get an exception with the string: "Run-time error"
 
 -- Part 2
 
@@ -169,6 +174,7 @@ data Bexp
   | NegBx Bexp
   | LeqAx Aexp Aexp
   | Bx Bool
+  | VarBx String
   deriving (Eq, Show)
 
 data Stm
@@ -197,6 +203,7 @@ compB bexp
   | (NegBx b) <- bexp = compB b ++ [Neg]
   | (LeqAx a1 a2) <- bexp = compA a2 ++ compA a1 ++ [Le]
   | (Bx b) <- bexp = if b then [Tru] else [Fals]
+  | (VarBx v) <- bexp = [Fetch v]
 
 compile :: Program -> Code
 compile [] = []
@@ -279,23 +286,21 @@ parseProdOrInt tokens =
 parseSumOrDiffOrProdOrInt :: [Token] -> Maybe (Aexp, [Token])
 parseSumOrDiffOrProdOrInt tokens =
   case parseProdOrInt tokens of
-    Just (expr1, TokPlus : restTokens1) -> case parseProdOrInt restTokens1 of
+    Just (expr1, TokPlus : restTokens1) -> case parseSumOrDiffOrProdOrInt restTokens1 of
       Just (expr2, restTokens2) -> Just (AddAx expr1 expr2, restTokens2)
       Nothing -> Nothing
-    Just (expr1, TokMinus : restTokens1) -> case parseProdOrInt restTokens1 of
+    Just (expr1, TokMinus : restTokens1) -> case parseSumOrDiffOrProdOrInt restTokens1 of
       Just (expr2, restTokens2) -> Just (SubAx expr1 expr2, restTokens2)
       Nothing -> Nothing
     result -> result
 
 parseIntOrParenExpr :: [Token] -> Maybe (Aexp, [Token])
-parseIntOrParenExpr (TokNum n : restTokens) = Just (NumAx n, restTokens)
-parseIntOrParenExpr (TokVar v : restTokens) = Just (VarAx v, restTokens)
 parseIntOrParenExpr (TokOpenBracket : restTokens1) =
   case parseSumOrDiffOrProdOrInt restTokens1 of
     Just (expr, TokCloseBracket : restTokens2) -> Just (expr, restTokens2)
     Just _ -> Nothing
     Nothing -> Nothing
-parseIntOrParenExpr tokens = Nothing
+parseIntOrParenExpr tokens = parseInt tokens
 
 parseProdOrIntOrPar :: [Token] -> Maybe (Aexp, [Token])
 parseProdOrIntOrPar tokens =
@@ -306,31 +311,119 @@ parseProdOrIntOrPar tokens =
         Nothing -> Nothing
     result -> result
 
-parseSumOrDifforProdOrIntOrPar :: [Token] -> Maybe (Aexp, [Token])
-parseSumOrDifforProdOrIntOrPar tokens =
+parseSumOrDiffOrProdOrIntOrPar :: [Token] -> Maybe (Aexp, [Token])
+parseSumOrDiffOrProdOrIntOrPar tokens =
   case parseProdOrIntOrPar tokens of
     Just (expr1, TokPlus : restTokens1) ->
-      case parseSumOrDifforProdOrIntOrPar restTokens1 of
+      case parseSumOrDiffOrProdOrIntOrPar restTokens1 of
         Just (expr2, restTokens2) -> Just (AddAx expr1 expr2, restTokens2)
         Nothing -> Nothing
     Just (expr1, TokMinus : restTokens1) ->
-      case parseSumOrDifforProdOrIntOrPar restTokens1 of
+      case parseSumOrDiffOrProdOrIntOrPar restTokens1 of
         Just (expr2, restTokens2) -> Just (SubAx expr1 expr2, restTokens2)
         Nothing -> Nothing
     result -> result
 
-buildArithmetic :: [Token] -> Aexp
-buildArithmetic tokens =
-  case parseSumOrDifforProdOrIntOrPar tokens of
+parseBool :: [Token] -> Maybe (Bexp, [Token])
+parseBool (TokTrue : restTokens) = Just (Bx True, restTokens)
+parseBool (TokFalse : restTokens) = Just (Bx False, restTokens)
+parseBool (TokVar v : restTokens) = Just (VarBx v, restTokens)
+parseBool tokens =
+  case parseSumOrDiffOrProdOrIntOrPar tokens of
+    Just (expr1, TokLeq : restTokens1) -> case parseSumOrDiffOrProdOrIntOrPar restTokens1 of
+      Just (expr2, restTokens2) -> Just (LeqAx expr1 expr2, restTokens2)
+    Just (expr1, TokAEq : restTokens1) -> case parseSumOrDiffOrProdOrIntOrPar restTokens1 of
+      Just (expr2, restTokens2) -> Just (EqAx expr1 expr2, restTokens2)
+    Nothing -> Nothing
+
+parseNotOrBool :: [Token] -> Maybe (Bexp, [Token])
+parseNotOrBool (TokNot : tokens) =
+  case parseBool tokens of
+    Just (expr1, restTokens1) -> Just (NegBx expr1, restTokens1)
+    result -> result
+parseNotOrBool tokens =
+  case parseBool tokens of
+    Just (expr1, restTokens1) -> Just (expr1, restTokens1)
+    result -> result
+
+parseEqOrNotOrBool :: [Token] -> Maybe (Bexp, [Token])
+parseEqOrNotOrBool tokens =
+  case parseNotOrBool tokens of
+    Just (expr1, TokBEq : restTokens1) -> case parseEqOrNotOrBool restTokens1 of
+      Just (expr2, restTokens2) -> Just (EqBx expr1 expr2, restTokens2)
+      Nothing -> Nothing
+    result -> result
+
+parseAndOrEqOrNotOrBool :: [Token] -> Maybe (Bexp, [Token])
+parseAndOrEqOrNotOrBool tokens =
+  case parseEqOrNotOrBool tokens of
+    Just (expr1, TokAnd : restTokens1) -> case parseAndOrEqOrNotOrBool restTokens1 of
+      Just (expr2, restTokens2) -> Just (AndBx expr1 expr2, restTokens2)
+      Nothing -> Nothing
+    result -> result
+
+parseBoolOrParExpr :: [Token] -> Maybe (Bexp, [Token])
+parseBoolOrParExpr (TokOpenBracket : restTokens1) =
+  case parseAndOrEqOrNotOrBool restTokens1 of
+    Just (expr, TokCloseBracket : restTokens2) -> Just (expr, restTokens2)
+    Just _ -> Nothing
+    Nothing -> Nothing
+parseBoolOrParExpr tokens = parseBool tokens
+
+parseNotOrBoolOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseNotOrBoolOrPar (TokNot : restTokens1) =
+  case parseBoolOrParExpr restTokens1 of
+    Just (expr, restTokens2) -> Just (NegBx expr, restTokens2)
+    Nothing -> Nothing
+parseNotOrBoolOrPar tokens = parseBool tokens
+
+parseBEqOrNotOrBoolOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseBEqOrNotOrBoolOrPar tokens =
+  case parseNotOrBoolOrPar tokens of
+    Just (expr1, TokBEq : restTokens1) -> case parseBEqOrNotOrBoolOrPar restTokens1 of
+      Just (expr2, restTokens2) -> Just (EqBx expr1 expr2, restTokens2)
+      Nothing -> Nothing
+    result -> result
+
+parseAndOrBEqOrNotOrBoolOrPar :: [Token] -> Maybe (Bexp, [Token])
+parseAndOrBEqOrNotOrBoolOrPar tokens =
+  case parseBEqOrNotOrBoolOrPar tokens of
+    Just (expr1, TokAnd : restTokens1) -> case parseAndOrBEqOrNotOrBoolOrPar restTokens1 of
+      Just (expr2, restTokens2) -> Just (AndBx expr1 expr2, restTokens2)
+      Nothing -> Nothing
+    result -> result
+
+buildBool :: [Token] -> Bexp
+buildBool tokens =
+  case parseAndOrBEqOrNotOrBoolOrPar tokens of
     Just (expr, []) -> expr
     _ -> error "Run-time error"
+
+buildArithmetic :: [Token] -> Aexp
+buildArithmetic tokens =
+  case parseSumOrDiffOrProdOrIntOrPar tokens of
+    Just (expr, []) -> expr
+    _ -> error "Run-time error"
+
+isBooleanExp :: [Token] -> Bool
+isBooleanExp [] = False
+isBooleanExp (exp : restTokens)
+  | exp `elem` [TokTrue, TokFalse, TokNot, TokAnd, TokAEq, TokBEq, TokLeq] = True
+  | otherwise = isBooleanExp restTokens
+
+isBooleanExp2 :: [Token] -> Bool
+isBooleanExp2 = any (\x -> x `elem` [TokTrue, TokFalse, TokNot, TokAnd, TokAEq, TokBEq, TokLeq])
+
+buildExpression :: [Token] -> String -> Stm
+buildExpression tokens var
+  | isBooleanExp2 tokens = AssignBx var (buildBool tokens)
+  | otherwise = AssignAx var (buildArithmetic tokens)
 
 buildData :: [Token] -> Program
 buildData [] = []
 buildData tokens
   | (TokSemicolon : restTokens1) <- tokens = buildData restTokens1
-  | (TokVar var : TokAssign : restTokens1) <- tokens = AssignAx var (buildArithmetic (takeWhile (/= TokSemicolon) restTokens1)) : buildData (dropWhile (/= TokSemicolon) restTokens1)
-
+  | (TokVar var : TokAssign : restTokens1) <- tokens = buildExpression (takeWhile (/= TokSemicolon) restTokens1) var : buildData (dropWhile (/= TokSemicolon) restTokens1)
 
 parse :: String -> Program
 parse = buildData . lexer
@@ -343,9 +436,14 @@ testParser programCode = (stack2Str stack, state2Str state)
 
 -- Examples:
 -- testParser "x := 5; x := x - 1;" == ("","x=4")
--- testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1 else y := 2;" == ("","y=2")
--- testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;)" == ("","x=1") -- o else devia acabar em ';'?
+-- testParser "x := 0 - 2;" == ("","x=-2")
+-- testParser "if (not True and 2 <= 5 = 3 == 4) then x :=1; else y := 2;" == ("","y=2")
+-- testParser "x := 42; if x <= 43 then x := 1; else (x := 33; x := x+1;);" == ("","x=1")
 -- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1;" == ("","x=2")
 -- testParser "x := 42; if x <= 43 then x := 1; else x := 33; x := x+1; z := x+x;" == ("","x=2,z=4")
+-- testParser "x := 44; if x <= 43 then x := 1; else (x := 33; x := x+1;); y := x*2;" == ("","x=34,y=68")
+-- testParser "x := 42; if x <= 43 then (x := 33; x := x+1;) else x := 1;" == ("","x=34")
+-- testParser "if (1 == 0+1 = 2+1 == 3) then x := 1; else x := 2;" == ("","x=1")
+-- testParser "if (1 == 0+1 = (2+1 == 4)) then x := 1; else x := 2;" == ("","x=2")
 -- testParser "x := 2; y := (x - 3)*(4 + 2*3); z := x +x*(2);" == ("","x=2,y=-10,z=6")
 -- testParser "i := 10; fact := 1; while (not(i == 1)) do (fact := fact * i; i := i - 1;);" == ("","fact=3628800,i=1")
